db587dc87e0a087f0adc065fd6dc9bbfa68220ed
code <code@advSoftware.prox>
2024-10-04 08:29:31 -0600

    added admin

--- cookapp/admin.py
+++ cookapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
 
 # Register your models here.
+# For customizing the admin site
+class MyAdminSite(admin.AdminSite):
+    site_header = 'CookApp Administration'
+    site_title = 'CookApp Admin'
+    index_title = 'CookApp Admin'
+
+admin_site = MyAdminSite(name='admin')
+
+# register the models with the admin site
+admin_site.register(User, UserAdmin)
+
+admin_site.register(Group, GroupAdmin)
