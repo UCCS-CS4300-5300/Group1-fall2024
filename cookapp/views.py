4bf4d4607411bfaec1f7e35ebaf27349ec1b706c
Tandon Jenkins <tjenkin5@uccs.edu>
2024-10-06 11:47:20 -0600

    new update script!

--- cookapp/views.py
+++ cookapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Ingredient, Recipe, UserPreference
 
 def index(request):
     return render(request, 'index.html')
--- update_db.sh
+++ update_db.sh
@@ -6,6 +6,17 @@ git pull
 # Remove the local database
 rm db.sqlite3
 
+# build venv
+python3 -m venv venv
+
+# Activate the virtual environment
+
+source venv/bin/activate
+
+# Install the requirements
+
+pip install -r requirements.txt
+
 # Recreate the database
 python manage.py migrate
 
