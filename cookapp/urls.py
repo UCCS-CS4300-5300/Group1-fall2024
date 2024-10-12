87a76b438c4cb8c87eda5f1b412f6e75c5ed17f5
Tandon Jenkins <tjenkin5@uccs.edu>
2024-10-12 13:39:35 -0600

    Added recipe search and added tags to API

--- cookapp/urls.py
+++ cookapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.RecipeSearch.as_view(), name='recipe_search'),
 ]
--- cookapp/views.py
+++ cookapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from .models import Ingredient, Recipe, UserPreference
+from django.http import JsonResponse
+from django.views import View
 
 def index(request):
     return render(request, 'index.html')
+
+class RecipeSearch(View):
+    def get(self, request):
+        query = request.GET.get('term', '')
+        
+        # Search by Recipe title or tags
+        recipe_results = Recipe.objects.filter(title__icontains=query)[:10]  # Limit results to 10
+        tag_results = Recipe.objects.filter(tags__icontains=query)[:10]  # Limit results to 10
+
+        # Prepare response data
+        recipe_list = list(recipe_results.values('title'))
+        tag_list = list(tag_results.values('title'))
+
+        # Combine and remove duplicates
+        combined_list = {item['title']: item for item in recipe_list + tag_list}.values()
+
+        return JsonResponse({
+            'recipes': list(combined_list),
+        }, safe=False)
