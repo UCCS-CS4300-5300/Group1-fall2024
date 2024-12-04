11dd00d9a5687c286ed57d3252a6535b10b377c3
Ron Lustig <ronlustig111@gmail.com>
2024-12-04 19:37:32 +0000

    Extra flake fixes

--- cookapp/forms.py
+++ cookapp/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Recipe, Ingredient, RecipeIngredient
-from django.forms import modelformset_factory
+from .models import Recipe, RecipeIngredient
+
 
 # Define forms for user creation, recipe, and recipe ingredients
 class CreateUserForm(UserCreationForm):
@@ -14,17 +14,25 @@ class CreateUserForm(UserCreationForm):
 class RecipeIngredientForm(forms.ModelForm):
     class Meta:
         model = RecipeIngredient
-        fields = ['ingredient', 'quantity']  # Replace with actual fields in the RecipeIngredient model
+        fields = ['ingredient', 'quantity']
+
+
 class RecipeForm(forms.ModelForm):
     calories = forms.IntegerField(label='Calories', required=True)
     protein = forms.IntegerField(label='Protein')
     carbs = forms.IntegerField(label='Carbohydrates')
     fat = forms.IntegerField(label='Fat')
-    tags = forms.CharField(label='Tags', help_text='Enter tags separated by commas')
+    tags = forms.CharField(
+        label='Tags',
+        help_text='Enter tags separated by commas'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['title', 'instructions', 'image', 'calories', 'protein', 'carbs', 'fat', 'tags']
+        fields = [
+            'title', 'instructions', 'image', 'calories',
+            'protein', 'carbs', 'fat', 'tags'
+        ]
 
     def clean_tags(self):
         tags = self.cleaned_data['tags']
@@ -46,7 +54,6 @@ class RecipeForm(forms.ModelForm):
         instance = super().save(commit=False)
         instance.macros = self.clean_macros()
         instance.tags = self.clean_tags()
-        
         if commit:
             instance.save()
         return instance
